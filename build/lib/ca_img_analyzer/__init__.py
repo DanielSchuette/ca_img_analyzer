c65247a2a608af21b8379925327cc9ce81382059
Daniel Schuette <d.schuette@online.de>
2018-10-25 22:48:07 +0200

    add demo .py files

---
+++ build/lib/ca_img_analyzer/__init__.py
@@ -0,0 +1,1 @@
+name = "ca_img_analyzer"
--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name="ca_img_analyzer",
     version="0.0.1",
-    description="Helper functions that facilitate the analysis of experimental calcium imaging data",
+    description="""Helper functions that facilitate the analysis
+                   of experimental calcium imaging data""",
     license="MIT",
     packages=["ca_img_analyzer"],
     author="Daniel Schuette",
     author_email="d.schuette@online.de",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     keywords=["Calcium Imaging"],
-    url="https://github.com/DanielSchuette/ca_img_analyzer")
+    url="https://github.com/DanielSchuette/ca_img_analyzer",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
