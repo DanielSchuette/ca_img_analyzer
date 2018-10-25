4c545677111e91ea209b5b7965d7b446c5795072
Daniel Schuette <d.schuette@online.de>
2018-10-25 22:05:12 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Calcium Imaging Analyzer
+`ca_img_analyzer` facilitates the analysis of experimental calcium imaging data.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="ca_img_analyzer",
+    version="0.0.1",
+    description="Helper functions that facilitate the analysis of experimental calcium imaging data",
+    license="MIT",
+    packages=["ca_img_analyzer"],
+    author="Daniel Schuette",
+    author_email="d.schuette@online.de",
+    keywords=["Calcium Imaging"],
+    url="https://github.com/DanielSchuette/ca_img_analyzer")
