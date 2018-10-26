1b12c85746f70373c505c1c927bc48b09ceaff07
Daniel Schuette <d.schuette@online.de>
2018-10-26 14:30:17 +0200

    update rate_of_rise.py

--- ca_img_analyzer/rate_of_rise.py
+++ ca_img_analyzer/rate_of_rise.py
@@ -1,2 +1,12 @@
+# rate_of_rise.py is part of the `ca_img_analyzer' package:
+# github.com/DanielSchuette/ca_img_analyzer
+#
+# this code is MIT licensed
+#
+# if you find a bug or want to contribute, please
+# use the GitHub repository or write an email:
+# d.schuette(at)online.de
+
+
 def greetings():
     print("rof")
--- ca_img_analyzer/stats.py
+++ ca_img_analyzer/stats.py
@@ -1,3 +1,11 @@
+# stats.py is part of the `ca_img_analyzer' package:
+# github.com/DanielSchuette/ca_img_analyzer
+#
+# this code is MIT licensed
+#
+# if you find a bug or want to contribute, please
+# use the GitHub repository or write an email:
+# d.schuette(at)online.de
 import numpy as np
 from scipy import integrate
 
@@ -18,9 +26,9 @@ def auc(df, column, rule="trapezoid"):
     # AUC using the trapezoidal rule
     if rule is "trapezoidal":
         area = np.trapz(arr, dx=5)
-        print("area =", area)
+        return area
 
     # Compute the area using the composite Simpson's rule.
     if rule is "simpson":
         area = integrate.simps(arr, dx=5)
-        print("area =", area)
+        return area
